7a0169d754852b31bdf03e4b417878fd599b247f
deriramdanis <deri.ramdani1@gmail.com>
2016-02-24 13:51:33 +0700

    Coba Commit

---
+++ mono-algorithms.coba/mono-algorithms.coba/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mono_algorithms.coba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
